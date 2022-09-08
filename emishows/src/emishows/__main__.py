45f2b88065c534b793e712ee52beff0afbdb4e97
Sebastian Pietras <spietras1998@gmail.com>
2022-09-08 10:23:05 +0200

    Secured database connection (#12)

--- emishows/src/emishows/__main__.py
+++ emishows/src/emishows/__main__.py
@@ -3,6 +3,7 @@
 This module provides basic CLI entrypoint.
 
 """
+import time
 
 import typer
 import uvicorn
@@ -15,6 +16,18 @@ from emishows.events import Calendar, calendars
 cli = typer.Typer()
 
 
+def wait_for_certificates() -> None:
+    filenames = ["ca.pem", "client.cert.pem", "client.key.pem"]
+    files = [config.certs_dir / filename for filename in filenames]
+
+    while not all(file.exists() for file in files):
+        typer.echo("Waiting for certificates...")
+        time.sleep(1)
+
+    with open("/etc/ssl/certs/ca-certificates.crt", "a") as f:
+        f.write(files[0].read_text())
+
+
 def create_calendar() -> None:
     calendars["emitimes"] = Calendar(
         url=f"http://{config.emitimes_host}:{config.emitimes_port}",
@@ -25,6 +38,7 @@ def create_calendar() -> None:
 
 
 def setup() -> None:
+    wait_for_certificates()
     call_command("migrate", "--no-input")
     create_calendar()
 
@@ -37,6 +51,7 @@ def main(
     port: int = typer.Option(default=35000, help="Port to run the server on"),
 ):
     """Command line interface for emishows."""
+
     setup()
     uvicorn.run(app, host=host, port=port)
 
