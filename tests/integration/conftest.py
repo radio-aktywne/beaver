22b8121fc09ae4082703419cacfe2b28f5f07957
Sebastian Pietras <me@spietras.dev>
2023-12-21 20:23:54 +0100

    Fetched template changes (#37)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,12 +1,17 @@
 import pytest
 import pytest_asyncio
+from httpx import AsyncClient, BasicAuth
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
+from prisma import Prisma
 
 from emishows.api.app import AppBuilder
 from emishows.config.builder import ConfigBuilder
 from emishows.config.models import Config
-from tests.utils.containers import AsyncDockerContainer, ContainerWaiter
+from tests.utils.containers import AsyncDockerContainer
+from tests.utils.waiting.conditions import CallableCondition
+from tests.utils.waiting.strategies import TimeoutStrategy
+from tests.utils.waiting.waiter import Waiter
 
 
 @pytest.fixture(scope="session")
@@ -24,8 +29,14 @@ def app(config: Config) -> Litestar:
 
 
 @pytest_asyncio.fixture(scope="session")
-async def database(config: Config) -> AsyncDockerContainer:
+async def database() -> AsyncDockerContainer:
     """Database container."""
+
+    async def _check() -> None:
+        async with Prisma(
+            datasource={"url": "postgres://user:password@localhost:34000/database"}
+        ):
+            return
 
     container = AsyncDockerContainer(
         "ghcr.io/radio-aktywne/apps/emishows-db:latest",
@@ -33,48 +44,37 @@ async def database(config: Config) -> AsyncDockerContainer:
         privileged=True,
     )
 
+    waiter = Waiter(
+        condition=CallableCondition(_check),
+        strategy=TimeoutStrategy(30),
+    )
+
     async with container as container:
-        # Wait for database to start.
-        waiter = ContainerWaiter(
-            container,
-            [
-                "./scripts/shell.sh",
-                "cockroachdb",
-                "sql",
-                "--certs-dir",
-                "data/certs/",
-                "--port",
-                str(config.database.port),
-                "--execute",
-                "''",
-            ],
-        )
         await waiter.wait()
         yield container
 
 
 @pytest_asyncio.fixture(scope="session")
-async def emitimes(config: Config) -> AsyncDockerContainer:
+async def emitimes() -> AsyncDockerContainer:
     """Emitimes container."""
+
+    async def _check() -> None:
+        auth = BasicAuth(username="user", password="password")
+        async with AsyncClient(base_url="http://localhost:36000", auth=auth) as client:
+            response = await client.get("/user/emitimes")
+            response.raise_for_status()
 
     container = AsyncDockerContainer(
         "ghcr.io/radio-aktywne/apps/emitimes:latest",
         network="host",
     )
 
+    waiter = Waiter(
+        condition=CallableCondition(_check),
+        strategy=TimeoutStrategy(30),
+    )
+
     async with container as container:
-        # Wait for emitimes to start.
-        waiter = ContainerWaiter(
-            container,
-            [
-                "./scripts/shell.sh",
-                "curl",
-                "--silent",
-                "--head",
-                "--fail",
-                f"http://localhost:{config.emitimes.port}",
-            ],
-        )
         await waiter.wait()
         yield container
 
